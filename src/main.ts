aecdc5c811d907de7c4bcb52a1e00ea8cb0c4928
toiq <tousif.iqbal@outlook.com>
2021-05-29 21:54:30 +0600

    Add sidebar transition and plugin setting tab

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,52 @@
-import { Plugin } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+interface MyPluginSettings {
+  leftSidebarAnimation: boolean;
+  rightSidebarAnimation: boolean;
+  leftSidebarWidth: number;
+  rightSidebarWidth: number;
+}
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+  leftSidebarAnimation: false,
+  rightSidebarAnimation: false,
+  leftSidebarWidth: 252,
+  rightSidebarWidth: 252,
+};
 
 export default class MyPlugin extends Plugin {
-  ribbon: HTMLElement;
-
-  sidebar: HTMLElement;
+  settings: MyPluginSettings;
+  leftRibbon: HTMLElement;
+  rightRibbon: HTMLElement;
+  leftSidebar: HTMLElement;
+  rightSidebar: HTMLElement;
 
   async onload() {
     // Initialize and set events when layout is fully ready
     this.app.workspace.onLayoutReady(() => {
       this.initialize();
       this.setEvents();
-      this.addStyle();
+      this.addSettingTab(new MyPluginSettingTab(this.app, this));
+      console.log('Values are: ' + this.settings);
+      this.loadSettings().then(() => {
+        this.addStyle();
+      });
     });
   }
 
   // Initializes the variables to store DOM HTML elements
   initialize: Function = () => {
-    this.ribbon = Array.from(
+    [this.leftRibbon, this.rightRibbon] = Array.from(
       document.getElementsByClassName(
         'workspace-ribbon'
       ) as HTMLCollectionOf<HTMLElement>
-    )[0];
+    );
 
-    this.sidebar = Array.from(
+    [this.leftSidebar, this.rightSidebar] = Array.from(
       document.getElementsByClassName(
-        'mod-left-split'
+        'mod-horizontal'
       ) as HTMLCollectionOf<HTMLElement>
-    )[0];
+    );
   };
 
   // Adds event listeners to the HTML elements
@@ -35,37 +55,161 @@ export default class MyPlugin extends Plugin {
       this.collapseSidebar();
     });
 
-    this.registerDomEvent(this.ribbon, 'mouseover', () => {
-      this.expandSidebar();
+    this.registerDomEvent(this.leftRibbon, 'mouseover', () => {
+      this.expandSidebar(this.leftSidebar);
+    });
+    this.registerDomEvent(this.rightRibbon, 'mouseover', () => {
+      this.expandSidebar(this.rightSidebar);
     });
 
-    this.registerDomEvent(this.sidebar, 'mouseover', () => {
-      this.expandSidebar();
+    this.registerDomEvent(this.leftSidebar, 'mouseover', () => {
+      this.expandSidebar(this.leftSidebar);
+    });
+
+    this.registerDomEvent(this.rightSidebar, 'mouseover', () => {
+      this.expandSidebar(this.rightSidebar);
     });
   };
 
   // Changes sidebar style width and display to expand it
-  expandSidebar = () => {
-    this.sidebar.style.width = '252px';
-    this.sidebar.style.removeProperty('display');
-    // document.body.addClass('.sidebar');
+  expandSidebar = (sidebar: HTMLElement) => {
+    try {
+      if (sidebar == this.leftSidebar) {
+        sidebar.style.width = String(this.settings.leftSidebarWidth) + 'px';
+        sidebar.style.removeProperty('display');
+      }
+      if (sidebar == this.rightSidebar) {
+        sidebar.style.width = String(this.settings.rightSidebarWidth) + 'px';
+        sidebar.style.removeProperty('display');
+      }
+    } catch (e) {
+      console.log('Failed to read property');
+    }
   };
 
   // Changes sidebar style width to collapse it
   collapseSidebar = () => {
-    this.sidebar.style.width = '0px';
+    this.leftSidebar.style.width = '0px';
+    this.rightSidebar.style.width = '0px';
   };
 
   // CSS for adding transition animation
   addStyle = () => {
-    this.sidebar.classList.add('sidebar');
-    this.sidebar.classList.toggle('.sidebar.active');
+    if (this.settings.leftSidebarAnimation) {
+      this.leftSidebar.classList.add('sidebar');
+      this.leftSidebar.classList.toggle('.sidebar.active');
+    }
+    if (this.settings.rightSidebarAnimation) {
+      this.rightSidebar.classList.add('sidebar');
+      this.rightSidebar.classList.toggle('.sidebar.active');
+    }
   };
 
   // Removes transition animation
   removeStyle = () => {
-    this.sidebar.classList.remove('sidebar');
+    if (!this.settings.leftSidebarAnimation) {
+      this.leftSidebar.classList.remove('sidebar');
+    }
+    if (!this.settings.rightSidebarAnimation) {
+      this.rightSidebar.classList.remove('sidebar');
+    }
   };
 
-  onunload() {}
+  onunload() {
+    this.saveSettings();
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign(DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+}
+
+// Plugin settings
+class MyPluginSettingTab extends PluginSettingTab {
+  plugin: MyPlugin;
+
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+    const { containerEl } = this;
+
+    containerEl.empty();
+
+    this.plugin.loadData();
+    containerEl.createEl('h2', { text: 'Sidebar Expand On Hover' });
+    containerEl.createEl('h4', { text: 'Sidebar Animation' });
+    const leftSidebarAnimation = new Setting(containerEl);
+    leftSidebarAnimation.setName('Left Sidebar');
+    leftSidebarAnimation.setDesc(
+      'Toggle to enable/disable left sidebar animation'
+    );
+    leftSidebarAnimation.addToggle((t) => {
+      t.setValue(this.plugin.settings.leftSidebarAnimation);
+      t.onChange(async (v) => {
+        if (v) {
+          this.plugin.settings.leftSidebarAnimation = true;
+          this.plugin.saveSettings();
+          this.plugin.addStyle();
+        } else {
+          this.plugin.settings.leftSidebarAnimation = false;
+          this.plugin.saveSettings();
+          this.plugin.removeStyle();
+        }
+      });
+    });
+
+    const rightSidebarAnimation = new Setting(containerEl);
+    rightSidebarAnimation.setName('Right Sidebar');
+    rightSidebarAnimation.setDesc(
+      'Toggle to enable/disable right sidebar animation'
+    );
+    rightSidebarAnimation.addToggle((t) => {
+      t.setValue(this.plugin.settings.rightSidebarAnimation);
+      t.onChange(async (v) => {
+        if (v) {
+          this.plugin.settings.rightSidebarAnimation = true;
+          this.plugin.saveSettings();
+          this.plugin.addStyle();
+        } else {
+          this.plugin.settings.rightSidebarAnimation = false;
+          this.plugin.saveSettings();
+          this.plugin.removeStyle();
+        }
+      });
+    });
+
+    containerEl.createEl('h4', { text: 'Sidebar Expand Width' });
+    const leftSidebarWidth = new Setting(containerEl);
+    leftSidebarWidth.setName('Left Sidebar');
+    leftSidebarWidth.setDesc('Set the width of left sidebar in pixel unit');
+    leftSidebarWidth.addText((t) =>
+      t
+        .setPlaceholder('Default: 252')
+        .setValue(String(this.plugin.settings.leftSidebarWidth))
+        .onChange(async (value) => {
+          this.plugin.settings.leftSidebarWidth = Number(value);
+          this.plugin.saveSettings();
+        })
+    );
+
+    const rightSidebarWidth = new Setting(containerEl);
+    rightSidebarWidth.setName('Right Sidebar');
+    rightSidebarWidth.setDesc('Set the width of right sidebar in pixel unit');
+    rightSidebarWidth.addText((t) =>
+      t
+        .setPlaceholder('Default: 252')
+        .setValue(String(this.plugin.settings.rightSidebarWidth))
+        .onChange(async (value) => {
+          this.plugin.settings.rightSidebarWidth = Number(value);
+          this.plugin.saveSettings();
+        })
+    );
+  }
 }
