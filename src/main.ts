4daed6f166d2ffec8bfcae4257f64b077adc9daa
toiq <tousif.iqbal@outlook.com>
2021-05-29 12:24:00 +0600

    Refactor code

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,58 @@
-import { Plugin, WorkspaceRibbon } from 'obsidian';
+import { Plugin } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
-  ribbon: any = null;
-  sidebar: any = null;
+  ribbon: HTMLElement;
+  sidebar: HTMLElement;
 
   async onload() {
     console.log('loading mouse hover expand plugin');
-    this.registerEvent(
-      this.app.workspace.on('layout-ready', () => {
-        this.initialize();
-        console.log('Got from layout-ready event');
-        this.setEvents();
-      })
-    );
-    try {
+
+    // Initialize and set events when layout is fully ready
+    this.app.workspace.onLayoutReady(() => {
       this.initialize();
       this.setEvents();
-    } finally {
-      window.setTimeout(() => {
-        this.initialize();
-        this.setEvents();
-      }, 2000);
-    }
+    });
   }
 
+  // Initializes the variables to store DOM HTML elements
   initialize: Function = () => {
     this.ribbon = Array.from(
       document.getElementsByClassName(
         'workspace-ribbon'
       ) as HTMLCollectionOf<HTMLElement>
-    );
+    )[0];
 
     this.sidebar = Array.from(
       document.getElementsByClassName(
         'mod-left-split'
       ) as HTMLCollectionOf<HTMLElement>
-    );
+    )[0];
   };
 
+  // Adds event listeners to the HTML elements
   setEvents: Function = () => {
     this.registerDomEvent(document, 'mouseout', () => {
-      this.sidebar[0].style.width = '0px';
+      this.collapseSidebar();
     });
 
-    this.registerDomEvent(this.ribbon[0] as HTMLElement, 'mouseover', () => {
-      this.sidebar[0].style.width = '266px';
-      this.sidebar[0].style.removeProperty('display');
+    this.registerDomEvent(this.ribbon, 'mouseover', () => {
+      this.expandSidebar();
     });
 
-    this.registerDomEvent(this.sidebar[0] as HTMLElement, 'mouseover', () => {
-      this.sidebar[0].style.width = '266px';
+    this.registerDomEvent(this.sidebar, 'mouseover', () => {
+      this.expandSidebar();
     });
+  };
+
+  // Changes sidebar style width and display to expand it
+  expandSidebar = () => {
+    this.sidebar.style.width = '252px';
+    this.sidebar.style.removeProperty('display');
+  };
+
+  // Changes sidebar style width to collapse it
+  collapseSidebar = () => {
+    this.sidebar.style.width = '0px';
   };
 
   onunload() {
